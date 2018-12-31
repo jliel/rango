d18fc61aecae1b33accd301b6f21ce3d44ea4431
jliel <kiritobaz@gmail.com>
2018-12-30 21:04:25 -0600

    Ahora es posible registrarse, con una imagen de perfil.

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('category/<slug:category_name_url>', views.category, name="category"),
     path('add_category/', views.add_category, name="add_category"),
     path('category/<slug:category_name_url>/add_page', views.add_page, name="add_page"),
+    path('register/',  views.register, name="register"),
 ]
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 # from django.http import HttpResponse
 from rango.models import Category, Page
-from rango.forms import Categoryform, PageForm
+from rango.forms import Categoryform, PageForm, UserForm, UserProfileForm
 
 
 def index(request):
@@ -37,13 +37,6 @@ def category(request, category_name_url):
         pass
     return render(request, 'rango/category.html', context_dict)
 
-
-def encode_url(category_name_url):
-    return category_name_url.replace('_', ' ')
-
-
-def decode_url(category_name):
-    return category_name.replace(' ', '_')
 
 def add_category(request):
     if request.method == "POST":
@@ -80,3 +73,36 @@ def add_page(request, category_name_url):
         'category_name': category_name,
         'form': form
     })
+
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors) 
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    return render(request, 'rango/register.html', {
+        'user_form': user_form, 'profile_form': profile_form, 'registered': registered
+    })
+
+
+def encode_url(category_name_url):
+    return category_name_url.replace('_', ' ')
+
+
+def decode_url(category_name):
+    return category_name.replace(' ', '_')
